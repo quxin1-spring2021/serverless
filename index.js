535b20cfee15b0b2fe3f0b421856e5ca68696d1d
Enoch Qu <enochqu@outlook.com>
2021-04-06 16:31:51 -0700

    update indexjs

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 console.log('Loading function');
 var aws = require("aws-sdk");
 var ses = new aws.SES({ region: "us-west-2" });
+let account = process.env.ACCOUNT;
 
 exports.handler = async function(event, context, callback) {
 // console.log('Received event:', JSON.stringify(event, null, 4));
 
 var sns = event.Records[0].Sns
 var message = sns.Message;
-var operation = sns.MessageAttributes.Operation;
-var Operation = sns.Operation;
-var Email = sns.Email;
-var email = sns.MessageAttributes.Email;
-console.log("Email" + Email);
-console.log("email" + email);
+var operation = sns.MessageAttributes.Operation.Value;
+var email = sns.MessageAttributes.Email.Value;
+
 console.log('Message received from SNS:', message);
 // callback(null, "Success");
 
 var params = {
     Destination: {
-      ToAddresses: [Email],
+      ToAddresses: [email],
     },
     Message: {
       Body: {
@@ -27,7 +25,7 @@ var params = {
 
       Subject: { Data: `Notification for ${operation} A Book Under Your Username` },
     },
-    Source: "test@dev.chuhsin.me",
+    Source: `notification@${account}.chuhsin.me`,
   };
   callback(null, "Success");
   return ses.sendEmail(params).promise()
