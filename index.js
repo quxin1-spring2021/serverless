f2f0c5f14b8d3a6aafd0d8a4bc60abcc802cc56a
Enoch Qu <enochqu@outlook.com>
2021-04-06 00:52:09 -0700

    ses module

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 console.log('Loading function');
+var aws = require("aws-sdk");
+var ses = new aws.SES({ region: "us-west-2" });
 
-exports.handler = function(event, context, callback) {
+exports.handler = async function(event, context, callback) {
 // console.log('Received event:', JSON.stringify(event, null, 4));
 
     var message = event.Records[0].Sns.Message;
