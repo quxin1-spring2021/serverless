01ad892e3038c4e4a8aff3e34fe049ffd27dde98
Enoch Qu <enochqu@outlook.com>
2021-04-07 17:40:06 -0700

    test

--- index.js
+++ index.js
@@ -8,11 +8,30 @@ exports.handler = async function(event, context, callback) {
 
 var sns = event.Records[0].Sns
 var message = sns.Message;
+console.log(sns)
 var operation = sns.MessageAttributes.Operation.Value;
 var email = sns.MessageAttributes.Email.Value;
 
 console.log('Message received from SNS:', message);
 // callback(null, "Success");
+var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+// var params_db = {
+//   TableName: 'messages',
+//   Key: {
+//     'KEY_NAME': {S: `${message.MessageID}`}
+//   },
+//   ProjectionExpression: 'ATTRIBUTE_NAME'
+// };
+
+// Call DynamoDB to read the item from the table
+ddb.getItem(params, function(err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", data.Item);
+  }
+});
 
 var params = {
     Destination: {
