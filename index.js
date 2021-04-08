703e7a5d2edb6c6b79a646a51ceda09477146fa5
Enoch Qu <enochqu@outlook.com>
2021-04-07 20:42:25 -0700

    dev

--- index.js
+++ index.js
@@ -3,68 +3,73 @@ var aws = require("aws-sdk");
 var ses = new aws.SES({ region: "us-west-2" });
 let account = process.env.ACCOUNT;
 
-exports.handler = async function(event, context, callback) {
-// console.log('Received event:', JSON.stringify(event, null, 4));
+exports.handler = async function (event, context, callback) {
+  // console.log('Received event:', JSON.stringify(event, null, 4));
 
-var sns = event.Records[0].Sns
-var message = sns.Message;
-console.log(sns)
-var operation = sns.MessageAttributes.Operation.Value;
-var email = sns.MessageAttributes.Email.Value;
-var bookid = sns.MessageAttributes.BookId.Value;
+  var sns = event.Records[0].Sns
+  var message = sns.Message;
+  console.log(sns)
+  var operation = sns.MessageAttributes.Operation.Value;
+  var email = sns.MessageAttributes.Email.Value;
+  var bookid = sns.MessageAttributes.BookId.Value;
 
-console.log('Message received from SNS:', message);
-// callback(null, "Success");
-var ddb = new aws.DynamoDB({apiVersion: '2012-08-10'});
+  console.log('Message received from SNS:', message);
+  // callback(null, "Success");
+  var ddb = new aws.DynamoDB({ apiVersion: '2012-08-10' });
 
-var params_db_get = {
-  TableName: 'messages',
-  Key: {
-    'MessageID': {S: `${sns.MessageId}`}
-  },
-};
+  var params_db_get = {
+    TableName: 'messages',
+    Key: {
+      'MessageID': { S: `${sns.MessageId}` }
+    },
+  };
 
-// Call DynamoDB to read the item from the table
-ddb.getItem(params_db_get, function(err, data) {
-  if (err) {
-    console.log("Success", err);
-  } else {
-    console.log("Error, this message already exist.", data);
-    return;
-  }
-});
-
-// If message ID doesn't exist, put new message id into dynamoDB
-
-var params_db_put = {
-  TableName: 'messages',
-  Item: {
-      'MessageID' : {S: `${sns.MessageId}`},
-      'BookID' : {S: `${bookid}`},
-      'Operation' : {S: 'Create'}
-  }
-  };    
-ddb.putItem(params_db_put, function(err, data) {
-  if (err) {
+  // Call DynamoDB to read the item from the table
+  var messageId = ddb.getItem(params_db_get, function (err, data) {
+    if (err) {
       console.log("Error", err);
-  } else {
+    } else {
       console.log("Success", data);
-  }
+    }
   });
 
-var params = {
-    Destination: {
-      ToAddresses: [email],
-    },
-    Message: {
-      Body: {
-        Text: { Data: `${message}` },
-      },
+  if (messageId.length() !== 0) {
+    console.log("This Message Already Existed.")
+    return;
+  } else {
 
-      Subject: { Data: `Notification for ${operation} A Book Under Your Username` },
-    },
-    Source: `notification@${account}.chuhsin.me`,
-  };
-  callback(null, "Success");
-  return ses.sendEmail(params).promise()
+    // If message ID doesn't exist, put new message id into dynamoDB
+
+    var params_db_put = {
+      TableName: 'messages',
+      Item: {
+        'MessageID': { S: `${sns.MessageId}` },
+        'BookID': { S: `${bookid}` },
+        'Operation': { S: 'Create' }
+      }
+    };
+    ddb.putItem(params_db_put, function (err, data) {
+      if (err) {
+        console.log("Error", err);
+      } else {
+        console.log("Success", data);
+      }
+    });
+
+    var params = {
+      Destination: {
+        ToAddresses: [email],
+      },
+      Message: {
+        Body: {
+          Text: { Data: `${message}` },
+        },
+
+        Subject: { Data: `Notification for ${operation} A Book Under Your Username` },
+      },
+      Source: `notification@${account}.chuhsin.me`,
+    };
+    callback(null, "Success");
+    return ses.sendEmail(params).promise()
+  }
 };
